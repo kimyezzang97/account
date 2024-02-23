17a901fd244925b1a0973a97da490292a4ba815b
kimyezzang97 <kimyezzang97@gmail.com>
2024-02-23 11:25:22 +0900

    redis spin lock 테스트

--- src/main/java/org/example/account/controller/AccountController.java
+++ src/main/java/org/example/account/controller/AccountController.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.example.account.domain.Account;
 import org.example.account.service.AccountService;
+import org.example.account.service.RedisTestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final RedisTestService redisTestService;
+
+    @GetMapping("/get-lock")
+    public String getLock(){
+        return redisTestService.getLock();
+    }
 
     @GetMapping("/create-account")
     public String createAccount(){
